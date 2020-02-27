48c5ce63d1e8c4429f038aa2ebe4171b90ff4543
Vithinn <vithinn@icloud.com>
2020-02-27 17:11:03 -0300

    token updated

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ bot.registry.registerGroup('enchantments', 'Enchantments');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
-bot.login('NjgyNDgxMDQ3MTYxNjY3NTg0.Xldoew.yWBPh8he9gTMEZr_Fd1ofTkA3qY');
+bot.login('NjgyNDgxMDQ3MTYxNjY3NTg0.XlgiJg.15kxY9FadXzETn9g490M6hMaeY0');
