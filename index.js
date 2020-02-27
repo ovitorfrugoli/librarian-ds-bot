c42d151a3e5346ee188b04b17c024809508379cf
Vithinn <vithinn@icloud.com>
2020-02-27 18:07:50 -0300

    token updated

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const commando = require('discord.js-commando');
 const bot = new commando.Client();
+const token = process.env.token;
 
 bot.registry.registerGroup('useful', 'Useful');
 bot.registry.registerGroup('enchantments', 'Enchantments');
 bot.registry.registerDefaults();
 bot.registry.registerCommandsIn(__dirname + "/commands");
 
-bot.login('NjgyNDgxMDQ3MTYxNjY3NTg0.XlgrZg.i8YJSmbo94jtRdVSn8MsvN-e1T4');
+bot.login(token);
